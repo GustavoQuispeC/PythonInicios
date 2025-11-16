9602a4322569877ea9522ad4fcf0bfeca4e51bf1
Gustavo <gusstavocta@gmail.com>
2025-11-15 23:49:47 -0500

    Primeros pasos con Python

---
+++ main.py
@@ -0,0 +1,21 @@
+print('Hola mundo')
+firt_name = "Codigo"
+last_name = 'facilito'
+print(firt_name)
+print(last_name)
+print(type(firt_name))
+print(type(last_name))
+#cadenas
+
+#number
+number = 100_000_000
+print(number)
+pi = 3.14
+print(pi)
+
+is_active = True
+print(is_active)
+print(type(is_active))
+number = 10
+result = number//10
+print(result)
