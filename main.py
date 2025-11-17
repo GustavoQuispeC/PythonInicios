c9ea2bd8525a78648929ffbe069e0f040328df3c
Gustavo <gusstavocta@gmail.com>
2025-11-16 23:55:42 -0500

    Aprendiendo a manipular listas

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 print('Hola mundo')
-firt_name = "Codigo"
+first_name = "Codigo"
 last_name = 'facilito'
-print(firt_name)
+print(first_name)
 print(last_name)
-print(type(firt_name))
+print(type(first_name))
 print(type(last_name))
 #cadenas
 
@@ -17,5 +17,24 @@ is_active = True
 print(is_active)
 print(type(is_active))
 number = 10
-result = number//10
+result = number/10
 print(result)
+print(type(result))
+
+
+#listas      0         1        2      3
+#          -4        -3       -2      -1
+fruits = ['manzana', 'pera', 'uva', 'naranja']
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+print(fruits[3])
+
+#value = fruits[len(fruits)-1]
+
+#otra forma de acceder al ultimo elemento
+value1 = fruits[-1]
+value2 = fruits[-2]
+
+print(value1)
+print(value2)
