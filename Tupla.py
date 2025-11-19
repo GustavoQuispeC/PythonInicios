64fed21c86e1d05b7bc9d99584e058daec9e2c0b
Gustavo <gusstavocta@gmail.com>
2025-11-18 23:00:49 -0500

    Trabajando con tuplas

---
+++ Tupla.py
@@ -0,0 +1,12 @@
+setting = ("Hola", 42, 3.14, True)
+print(setting)
+print(type(setting))
+print(setting[0])
+print(setting[1])
+print(setting[2])
+print(setting[3])
+print(type(setting))
+
+courses = ('MongoDB', 'Java', 'NextJS', 'C#' )
+var1, var2, var3, var4 = courses
+print(var1,var2,var3,var4)
