a21f4f77c97db3b28174091d1337ef37f0eeaddb
Gustavo <gusstavocta@gmail.com>
2025-11-20 23:27:57 -0500

    manipulando string

---
+++ String.py
@@ -0,0 +1,15 @@
+mensaje = 'Hola mundo!'
+print(mensaje)
+print('?' in mensaje)
+print(len(mensaje))
+print(mensaje.index('!'))
+print(mensaje[10])
+
+#Aplicando slicing
+mensaje2 = mensaje[1:]
+print(mensaje2)
+print('h' + mensaje2)
+
+print(mensaje.count('o'))
+print(mensaje.upper())
+print(mensaje.lower())
